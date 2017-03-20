76b2c58d98dbf34dc99a4abc5269e081162a5735
SySarmad <syed.sarmad15@gmail.com>
2017-03-19 17:57:16 -0700

    admin and register for all models

--- RideCellParkingApi/admin.py
+++ RideCellParkingApi/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from RideCellParkingApi.models import Places, AllParkingSpots, AvailableParkingSpots, ReservedParkingSpots
 
-# Register your models here.
+class PlacesAdmin(admin.ModelAdmin):
+    list_display = ('lat', 'lng')
+
+class AllParkingSpotAdmin(admin.ModelAdmin):
+    list_display = ('p_id', 'lat', 'lng')
+
+class AvailableParkingSpotsAdmin(admin.ModelAdmin):
+    list_display = ('parking_id', 'place_id', 'radius')
+
+class ReservedParkingSpotsAdmin(admin.ModelAdmin):
+    list_display = ('parking_id', 'time_range')
+
+
+admin.site.register(Places, PlacesAdmin)
+admin.site.register(AllParkingSpots, AllParkingSpotAdmin)
+admin.site.register(AvailableParkingSpots, AvailableParkingSpotsAdmin)
+admin.site.register(ReservedParkingSpots, ReservedParkingSpotsAdmin)
