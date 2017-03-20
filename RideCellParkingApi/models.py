ec035fb5ebb37906fa161a0e00775d1f60dd9db9
SySarmad <syed.sarmad15@gmail.com>
2017-03-19 17:46:04 -0700

    Places, AllParkingSpots, AvailableParkingSpots, ReserverdParkingSpots base Models

--- RideCellParkingApi/models.py
+++ RideCellParkingApi/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Places(models.Model):
+    lat = models.DecimalField(max_digits=10, decimal_places=8)
+    lng = models.DecimalField(max_digits=11, decimal_places=8)
+
+    def __str__(self):
+        return "{}, {}".format(self.lat, self.lng)
+
+class AllParkingSpots(models.Model):
+    p_id = models.SlugField(max_length=6, primary_key=True, null=False)
+    lat = models.DecimalField(max_digits=10, decimal_places=8)
+    lng = models.DecimalField(max_digits=11, decimal_places=8)
+
+    def __str__(self):
+        return "{}, {}, {}".format(self.p_id, self.lat, self.lng)
+
+
+class AvailableParkingSpots(models.Model):
+    parking_id = models.ForeignKey(AllParkingSpots)
+    place_id = models.ForeignKey(Places)
+    radius = models.IntegerField(max_length=10, null=False)
+
+    def __str__(self):
+        return "{}, {}, {}".format(self.parking_id, self.place_id, self.radius)
+
+class reserved_parking_spots(models.Model):
+    parking_id = models.OneToOneField(AvailableParkingSpots)
+    time_range = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return "{}, {}".format(self.parking_id, self.time_range)
