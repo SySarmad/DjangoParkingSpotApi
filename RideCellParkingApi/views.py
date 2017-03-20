2b312c25ce3b27ff02f34e9c9767f1d44f9ce5df
SySarmad <syed.sarmad15@gmail.com>
2017-03-19 19:08:08 -0700

    added methods add_place, and find_and_add_all_parking_spots

--- RideCellParkingApi/views.py
+++ RideCellParkingApi/views.py
@@ -21,13 +21,29 @@ def haversine(lon1, lat1, lon2, lat2):
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
 
-def add_parking_spots(place_id, parkingspot_id, distance):
+def add_parking_spots_to_place(place_id, parkingspot_id, distance):
+    """Adds a parking Spot to the table"""
     new_parking_spot = AvailableParkingSpots(p_id=parkingspot_id, place_id=place_id, distance=distance)
+    new_parking_spot.save()
 
+def add_place(lat, lng):
+    places = Places(lat=lat, lng=lng)
+    places.save()
+    p_id = Places.objects.get(lat=lat, lng=lng).id
+    return p_id
+
+def find_and_add_all_parking_spots(place_id, lat, lng, radius):
+    """This finds all parking spots within r and adds it to the list"""
+    all_spots = AllParkingSpots.objects.all()
+    for i in all_spots:
+        if haversine(lat, lng, i[1], i[2]) <= radius:
+            new_spot = AvailableParkingSpots(parking_id=i.id, place_id=place_id, distance=radius)
 
 
 def get_all_available_parking_spots(lat, lng, radius):
-    places = Places(lat=lat, lng=lng).save()
-    p_id = Places.objects.get(lat=lat, lng=lng).id
-    available_parking_spots = AvailableParkingSpots.objects.filter(distance_lte=radius)
+    p_id = add_place(lat, lng)
+    available_parking_spots = AvailableParkingSpots.objects.filter(place_id=p_id, distance_lte=radius)
+
+    if len(available_parking_spots) == 0:
+        find_and_add_all_parking_spots(p_id, lat, lng, radius)
     return available_parking_spots
